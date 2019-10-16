5259e25d7bc84e8506a7479aa8ccdaa7bc6d15d4
Will Keller <wkeller3@binghamton.edu>
2019-10-16 10:41:18 -0400

    Added files

---
+++ Project4/Project4/GameScene.swift
@@ -0,0 +1,38 @@
+//
+//  GameScene.swift
+//  Project4
+//
+//  Created by Will Keller on 10/16/19.
+//  Copyright © 2019 Will Keller. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
---
+++ Project4/Project4/MainMenuViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MainMenuViewController.swift
+//  Project4
+//
+//  Created by Will Keller on 10/16/19.
+//  Copyright © 2019 Will Keller. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class MainMenuViewController: UIViewController {
+
+    
+    @IBOutlet weak var playButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
