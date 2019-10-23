b11092e27f2d19c9c830de8412d934520d0d5e20
Will Keller <wkeller3@binghamton.edu>
2019-10-23 19:16:38 -0400

    Nodes out of view now get deleted

--- Project4/Project4/GameScene.swift
+++ Project4/Project4/GameScene.swift
@@ -16,24 +16,26 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let cam = SKCameraNode();
     var flagR:Bool = false;
     var flagL:Bool = false;
+    var jump:Bool = false;
     
     override func didMove(to view: SKView) {
         self.physicsWorld.contactDelegate = self;
         self.player = self.childNode(withName: "player") as? SKSpriteNode;
         self.floor = self.childNode(withName: "floor") as? SKSpriteNode;
+        player!.physicsBody?.contactTestBitMask = player!.physicsBody!.collisionBitMask;
         self.camera = cam;
-        print("BEGIN");
         
     }
     
     func didBegin(_ contact: SKPhysicsContact){
-        print("TEST");
+        jump = true;
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         let location = touches.first?.location(in: self.view);
-        if(location!.y < (self.view?.frame.height)!/2){
+        if(location!.y < (self.view?.frame.height)!/2 && jump){
             player?.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 50));
+            jump = false;
         } else if(location!.x > (self.view?.frame.width)!/2){
             flagR = true;
             //player?.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 0));
@@ -66,5 +68,16 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             player?.physicsBody?.applyImpulse(CGVector(dx: -5, dy: 0));
         }
         cam.position = CGPoint(x: player!.position.x+500, y: player!.position.y+245);
+        if(player!.position.x > CGFloat(775)){
+            floor?.removeFromParent();
+            self.childNode(withName: "stair1")?.removeFromParent();
+            self.childNode(withName: "stair2")?.removeFromParent();
+            self.childNode(withName: "stair3")?.removeFromParent();
+            self.childNode(withName: "stair4")?.removeFromParent();
+            self.childNode(withName: "stair5")?.removeFromParent();
+            self.childNode(withName: "stair6")?.removeFromParent();
+            self.childNode(withName: "stair7")?.removeFromParent();
+            self.childNode(withName: "stair8")?.removeFromParent();
+        }
     }
 }
