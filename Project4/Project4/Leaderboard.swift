ed71a3f310f5421d9f94ef5e5ab043e5266ab72a
Will Keller <wkeller3@binghamton.edu>
2019-10-27 00:50:14 -0400

    Updated files

--- Project4/Project4/Leaderboard.swift
+++ Project4/Project4/Leaderboard.swift
@@ -8,67 +8,9 @@
 
 import Foundation
 import UIKit
+import SpriteKit
 
-class Leaderboard: UIViewController {
+class Leaderboard: SKScene {
 
-    var myCustomViewController: GameViewController = GameViewController(nibName: nil, bundle: nil)
-
-    let labelOne: UILabel = {
-        let label = UILabel()
-        label.text = ""
-        label.backgroundColor = .red
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    let labelTwo: UILabel = {
-        let label = UILabel()
-        label.text = "Scroll Bottom"
-        label.backgroundColor = .green
-        label.translatesAutoresizingMaskIntoConstraints = false
-        return label
-    }()
-
-    let scrollView: UIScrollView = {
-        let v = UIScrollView()
-        v.translatesAutoresizingMaskIntoConstraints = false
-        v.backgroundColor = .cyan
-        return v
-    }()
-
-
-override func viewDidLoad() {
-    super.viewDidLoad()
     
-    // add the scroll view to self.view
-    self.view.addSubview(scrollView)
-    // constrain the scroll view to 8-pts on each side
-    scrollView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 8.0).isActive = true
-    scrollView.topAnchor.constraint(equalTo: view.topAnchor, constant: 8.0).isActive = true
-    scrollView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -8.0).isActive = true
-    scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -8.0).isActive = true
-
-    // add labelOne to the scroll view
-    scrollView.addSubview(labelOne)
-
-    // constrain labelOne to left & top with 16-pts padding
-    // this also defines the left & top of the scroll content
-    labelOne.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 16.0).isActive = true
-    labelOne.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 16.0).isActive = true
-
-    // add labelTwo to the scroll view
-    scrollView.addSubview(labelTwo)
-
-    // constrain labelTwo at 400-pts from the left
-    labelTwo.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor, constant: 400.0).isActive = true
-
-    // constrain labelTwo at 1000-pts from the top
-    labelTwo.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 1000).isActive = true
-
-    // constrain labelTwo to right & bottom with 16-pts padding
-    labelTwo.rightAnchor.constraint(equalTo: scrollView.rightAnchor, constant: -16.0).isActive = true
-            labelTwo.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -16.0).isActive = true
-
-        }
-
-    }
+}
